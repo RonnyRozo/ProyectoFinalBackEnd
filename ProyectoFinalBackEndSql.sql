b36967dcbd40bedbd612b5c2549473b619aefbea
RonnyRozo <134233689+RonnyRozo@users.noreply.github.com>
2023-09-06 14:33:58 -0500

    Add files via upload

---
+++ ProyectoFinalBackEndSql.sql
@@ -0,0 +1,19 @@
+CREATE TABLE descripcion (
+	nombre_producto VARCHAR(20),
+	descripcion_producto VARCHAR(30),
+	precio INT, 
+	SKU INT, 
+	
+);
+
+CREATE TABLE compradores (
+	nombre VARCHAR(20),
+	apellido  VARCHAR(20),
+	email VARCHAR(40),
+	telefono INT, 
+	direccion_completa VARCHAR(40),
+	codigo_postal INT,
+	barrio VARCHAR(40),
+
+);
+
